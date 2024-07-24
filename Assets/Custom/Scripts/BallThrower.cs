d35c6b37c89a2fdb576dd57996d1ae4bce77e4ee
HennysTous <tf2hennys@gmail.com>
2024-07-24 02:06:29 -0500

    Lava Shader and Ball Throw

--- Assets/Custom/Scripts/BallThrower.cs
+++ Assets/Custom/Scripts/BallThrower.cs
@@ -2,77 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallThrower : MonoBehaviour {
-
-	Vector2 startPos, endPos, direction; // touch start position, touch end position, swipe direction
-	float touchTimeStart, touchTimeFinish, timeInterval; // to calculate swipe time to sontrol throw force in Z direction
-
+public class BallThrower : MonoBehaviour
+{
 	[SerializeField]
-	float throwForceInXandY = 1f; // to control throw force in X and Y directions
+	float throwForce = 2f; // to control throw force in Z direction
 
-	[SerializeField]
-	float throwForceInZ = 50f; // to control throw force in Z direction
+	GameObject ball;
 
+	public bool isPicked = false;
 	Rigidbody rb;
 
-    ObjectPicker objectPicker;
-
-    GameObject XROrigin;
-
-    void Awake()
-    {
-        XROrigin = GameObject.Find("XR Origin");
-        objectPicker = XROrigin.GetComponent<ObjectPicker>();
-    }
-	void Start()
+	// Update is called once per frame
+	void Update()
 	{
+		if (isPicked) ball.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.1f, Camera.main.nearClipPlane * 7.5f));
+
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+
+			if(isPicked) Throw(); else Pick();
+
+		}
 		
 
 	}
 
-	// Update is called once per frame
-	void Update () {
+	void Throw(){
 
-        if(objectPicker.isCatched == true){
-
-            rb = objectPicker.obj.GetComponent<Rigidbody> ();
-
-        }
-
-		// if you touch the screen
-		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began && objectPicker.isCatched == true) {
-
-			// getting touch position and marking time when you touch the screen
-            objectPicker.enabled = false;
-			touchTimeStart = Time.time;
-			startPos = Input.GetTouch (0).position;
-		}
-
-		// if you release your finger
-		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended) {
-
-			// marking time when you release it
-			touchTimeFinish = Time.time;
-
-			// calculate swipe time interval 
-			timeInterval = touchTimeFinish - touchTimeStart;
-
-			// getting release finger position
-			endPos = Input.GetTouch (0).position;
-
-			// calculating swipe direction in 2D space
-			direction = startPos - endPos;
-
-			// add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
+			isPicked = false;
 			rb.isKinematic = false;
-            objectPicker.enabled = true;
-            objectPicker.isCatched = false;
-			rb.AddForce (- direction.x * throwForceInXandY, - direction.y * throwForceInXandY, throwForceInZ / timeInterval);
-
-			// Destroy ball in 4 seconds
-			Destroy (gameObject, 4f);
-
-		}
-			
+			rb.useGravity = true;
+			rb.AddForce(Camera.main.transform.forward * throwForce, ForceMode.Impulse);
 	}
+
+	void Pick(){
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit _hit;
+
+			if (Physics.Raycast(ray, out _hit, 100f))
+			{
+				if (_hit.transform.tag == "Throwable")
+				{
+					isPicked = true;
+					ball = _hit.transform.gameObject;
+					rb = ball.GetComponent<Rigidbody>();
+					ball.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.1f, Camera.main.nearClipPlane * 7.5f));
+
+					rb.isKinematic = true;
+					rb.useGravity = false;
+
+				}
+			}
+
+	}
+
 }
